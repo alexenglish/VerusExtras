9a6c17126f243e27fb2ba0255383431be90a86d2
Oink70 <oink1970@hotmail.com>
2021-03-15 12:19:44 +0000

    Added private addresses and formatted  layout

--- listaddressbalances.sh
+++ listaddressbalances.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#Copyright Alex English November 2019
+#Copyright Alex English November 2019 
 #This script comes with no warranty whatsoever. Use at your own risk.                                                        
-#Tested on Ubuntu 17.10 and Debian 10
-
+#Tested on Ubuntu 17.10 and 18.04
 SHOWEMPTY=$1
 
 #lists balances for each address that has any unspent transactions
@@ -18,7 +17,9 @@ if ! command -v jq > /dev/null ; then
 	exit 1
 fi
 
-$VERUS listunspent 0 999999999 | jq -cr '.[]|[.address,.amount]' | tr -d '[]"' | tr ',' "\t" | awk '{x[$1]+=$2*1.0} END {for (key in x) { print key "\t" x[key] }}' | while read L; do
+printf "\n%-78s %17s \n" Address Balance
+printf "%78s %17s \n" ------------------------------------------------------------------------------ -----------------
+$VERUS listunspent 0 999999999 | jq -cr '.[]|[.address,.amount]' | tr -d '[]"' | tr ',' "\t" | awk '{x[$1]+=$2*1.0} END {for (key in x) { printf "%s %17.8f \n", key, x[key] }}' | while read L; do
 	ADDR="$(awk '{print $1}'<<<"$L")"
 	AMT="$(awk '{print $2}'<<<"$L")"
 
@@ -32,5 +33,7 @@ $VERUS listunspent 0 999999999 | jq -cr '.[]|[.address,.amount]' | tr -d '[]"' |
 		ADDR="$($VERUS getidentity "$ADDR" | jq -r '.identity.name')@"
 	fi
 
-	printf "$ADDR:%.8f\n" "$AMT"
+	printf "%-78s %17.8f\n" "$ADDR" $AMT
 done | column -s : -t
+$VERUS z_listunspent 0 999999999 | jq -cr '.[]|[.address,.amount]' | tr -d '[]"' | tr ',' "\t" | awk '{x[$1]+=$2} END {for (key in x) { printf "%-78s %17.8f \n", key, x[key] }}'
+printf "%78s %17s \n\n" ------------------------------------------------------------------------------ -----------------
