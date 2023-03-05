cf8f3cec38ae3a017e0e74ce780debb0546ff084
alexenglish <alexenglish@users.noreply.github.com>
2023-03-05 09:30:52 -0800

    Removed broken show zero balances option, removed non-data output that made it less well suited to scripting.

--- listaddressbalances.sh
+++ listaddressbalances.sh
@@ -3,7 +3,8 @@
 #Copyright Alex English November 2019 
 #This script comes with no warranty whatsoever. Use at your own risk.                                                        
 #Tested on Ubuntu 17.10 and 18.04
-SHOWEMPTY=$1
+
+#note that the output can be sorted by value by piping to `sort -nk3` or `sort -rnk3` for an ascending or descenting sort, respectively
 
 #lists balances for each address that has any unspent transactions
 if ! source "$( dirname "${BASH_SOURCE[0]}" )"/config; then
@@ -17,16 +18,9 @@ if ! command -v jq > /dev/null ; then
 	exit 1
 fi
 
-printf "\e[33m%-78s %17s \e[0m\n" Address Balance
-printf "%78s %17s \n" ------------------------------------------------------------------------------ -----------------
 $VERUS listunspent 0 999999999 | jq -cr '.[]|[.address,.amount]' | tr -d '[]"' | tr ',' "\t" | awk '{x[$1]+=$2*1.0} END {for (key in x) { printf "%s %17.8f \n", key, x[key] }}' | while read L; do
 	ADDR="$(awk '{print $1}'<<<"$L")"
 	AMT="$(awk '{print $2}'<<<"$L")"
-
-	#if it is empty skip unless SHOWEMPTY is set
-	if [ "$AMT" = "0.00000000" ] && [ -z "$SHOWEMPTY" ]; then
-		continue
-	fi
 
 	#if it's an iaddress resolve the identity name
 	if [[ "$ADDR" =~ ^i ]]; then
@@ -40,11 +34,5 @@ $VERUS z_listunspent 0 999999999 | jq -cr '.[]|[.address,.amount]' | tr -d '[]"'
 	ADDR="$(awk '{print $1}'<<<"$L")"
 	AMT="$(awk '{print $2}'<<<"$L")"
 
-	#if it is empty skip unless SHOWEMPTY is set
-	if [ "$AMT" = "0.00000000" ] && [ -z "$SHOWEMPTY" ]; then
-		continue
-	fi
-
 	printf "\e[36m%-78s \e[33m%17.8f\e[0m\n" "$ADDR" $AMT
 done | column -s : -t
-printf "%78s %17s \n\n" ------------------------------------------------------------------------------ -----------------
