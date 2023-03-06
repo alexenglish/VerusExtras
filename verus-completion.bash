94dcbedfc2a931d3269fb5bf41d2459e408aa03d
alexenglish <alexenglish@users.noreply.github.com>
2023-03-05 23:07:03 -0800

    Updated bash and zsh completion scripts with all RPC commands from help.

--- verus-completion.bash
+++ verus-completion.bash
@@ -3,12 +3,12 @@
 #This script comes with no warranty whatsoever. Use at your own risk.
 #Tested on Ubuntu 18.04
 
-pushd "$( dirname "${BASH_SOURCE[0]}" )" 
+pushd "$( dirname "${BASH_SOURCE[0]}" )" &>/dev/null
 
 #source verus-completions.bash in your bash shell to make use of it. ( run "source verus-completions.bash" )
 #or source this file in your .bashrc file
 source completion_vars
 
-complete -W "$CONTROL $NETWORK $BLOCKCHAIN $CROSSCHAIN $MINING $GENERATING $RAWTRANSACTIONS $ADDRESSINDEX $UTIL $HIDDEN $WALLET $DISCLOSURE $PBAAS" verus komodo-cli
+complete -W "$CONTROL $NETWORK $BLOCKCHAIN $CROSSCHAIN $MINING $GENERATING $RAWTRANSACTIONS $ADDRESSINDEX $UTIL $WALLET $DISCLOSURE $PBAAS $IDENTITY $VDXF $MARKETPLACE $MULTICHAIN" verus
 
-popd
+popd &>/dev/null
--- verus-completion.zsh
+++ verus-completion.zsh
@@ -1,19 +1,21 @@
-#!/bin/zsh
+#!/bin/env zsh
 #Copyright Alex English May 2019
 #This script comes with no warranty whatsoever. Use at your own risk.
-#Tested on Ubuntu 18.04
-
-pushd "$( dirname "${BASH_SOURCE[0]}" )" 
+#Updated March 2023
 
 #source this file in your .zshrc or run "source verus-completion.zsh" from your zsh shell
+#your .zshrc must contain these lines before sourcing this script
+#autoload -Uz compinit
+#compinit
+
+pushd "${0:a:h}" 
 
 source completion_vars
 
 function _verus {
-    _arguments -C "1: :($CONTROL $NETWORK $BLOCKCHAIN $CROSSCHAIN $MINING $GENERATING $RAWTRANSACTIONS $ADDRESSINDEX $UTIL $HIDDEN $WALLET $DISCLOSURE $PBAAS)"
+    _arguments -C "1: :($CONTROL $NETWORK $BLOCKCHAIN $CROSSCHAIN $MINING $GENERATING $RAWTRANSACTIONS $ADDRESSINDEX $UTIL $WALLET $DISCLOSURE $PBAAS $IDENTITY $VDXF $MARKETPLACE $MULTICHAIN)"
 }
 
 compdef _verus verus
-compdef _verus komodo-cli
 
 popd
