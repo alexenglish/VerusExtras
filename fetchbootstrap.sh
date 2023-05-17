e31d34a9fc2a68ecfa4ca831f9d4180cb207ec38
alexenglish <alexenglish@users.noreply.github.com>
2023-05-17 12:16:07 -0700

    Include checksums in bootstrap download

--- fetchbootstrap.sh
+++ fetchbootstrap.sh
@@ -2,6 +2,8 @@
 
 if command -v wget &>/dev/null ; then
     wget https://bootstrap.verus.io/VRSC-bootstrap.tar.gz
+    wget https://bootstrap.verus.io/VRSC-bootstrap.tar.gz.sha256sum
 else
     curl https://bootstrap.verus.io/VRSC-bootstrap.tar.gz --output VRSC-bootstrap.tar.gz
+    curl https://bootstrap.verus.io/VRSC-bootstrap.tar.gz.sha256sum --output VRSC-bootstrap.tar.gz.sha256sum
 fi
