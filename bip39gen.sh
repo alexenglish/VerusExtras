5950c0ddcf8c47e1dfb7006f233559240e1b8ee0
alexenglish <alexenglish@users.noreply.github.com>
2023-09-14 11:06:46 -0700

    Added functions to simplify dependency checks, improved bip39gen with some simplifications and better help text.

--- bip39gen.sh
+++ bip39gen.sh
@@ -15,12 +15,13 @@ fi
 
 cat <<EOF 1>&2
 Use with caution - only trust this seed phrase generation if you trust the randomness available on the device you're running it on.
-Please enter some text, numbers, and/or symbols as a bit of personal entropy.
+Please enter some text, numbers, and/or symbols as a bit of personal entropy, as much as you like, pressing Enter/return will end the collection.
 This will be contributed to the entropy in your operating system, it is not the primary basis for your passphrase.
 What you enter will be of no use in the future. Your security will be best if you do not enter anything memorable and do not document it.
-You can also pipe something into this script rather than entering text yourself, just perhaps don't pipe in random output from your OS or you're defeating the purpose.
 If you leave this empty you'll still have all the quality of randomness your OS provides, which is probably sufficient on its own.
 This is primarily for safety on low-entropy systems like some single-board computers. Review the the source of this file and use at your own risk.
+
+To use this seed phrase to generate a Verus R-address (and public/private keys), use \`verus convertpassphrase "<24-word seed phrase>"\`.
 
 ->
 EOF
@@ -28,16 +29,15 @@ EOF
 read ENT
 message
 
-#keyboard interrupt timing while typing added to entropy - supply the text itself as entropy
+#keyboard interrupt timing while typing added to entropy along with all the deeper OS factors - network packet timing, device statuses and IDs, device temperatures, etc.
+# supply the text itself as entropy
 cat <<<"$ENT" > /dev/random
-#supply the hash of a random readable file from /tmp as entropy
-sha512sum "$(find /tmp -readable -type f 2>/dev/null | shuf -n1)" 2>/dev/random | cut -d' ' -f1 > /dev/random
 
 U=3
 #select random words
 #loop until we don't have more than two occurrences of a word in our phrase (this is usually the first time)
 while [ $U -gt 2 ]; do
-	WORDS=$(for N in `seq 1 2048`; do shuf "${VEPATH}/lib/bip39list.txt"; done | shuf | shuf --random-source=/dev/random -n 24)
+	WORDS=$(for N in `seq 1 2048`; do shuf "${VEPATH}/lib/bip39list.txt" --random-source=/dev/random ; done | shuf --random-source=/dev/random | shuf --random-source=/dev/random -n 24)
 	U=$(sort <<<"$WORDS" | uniq -c | awk '{print $1}' | sort -n | head -n1)
 done
 
